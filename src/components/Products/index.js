d3e05d82367b068cfb799a3e6b7f668c10d86fc2
magne-matias <magnematias01@gmail.com>
2021-06-10 18:49:43 -0300

    commit

--- src/components/Products/index.js
+++ src/components/Products/index.js
@@ -1,10 +1,32 @@
 import React from 'react'
+import Img from "../../images/g1.jpg"
+
 
 export const ProductsList = () => {
     return (
-        <div>
-            <h1>Lista de productos</h1>
+    <>{/*remember bro; esto sirve para que no retorne al pader solo a los hijos */}
+        <h1 className="title">PRODUCTOS</h1>
+        <div className="Productos">
+            <div className="producto">
+                <a href="">
+                    <div className="producto_img">
+                        <img src={Img} alt="img" width="200px" />
+                    </div>
+                </a>
+                <div className="producto_footer">
+                    <h1>title</h1>
+                    <p>categoria</p>
+                    <p className="price">$320</p>
+                </div>
+                <div className="button">
+                    <button className="btn">Añadir al carrito </button>
+                </div>
+                <div>
+                    <a href="#" className="btn">Vista</a>
+                </div>
+            </div>
         </div>
+    </>
     )
 }
 
