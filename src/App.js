969e640fb2ebc8c6bb891674ad0539fdc9cbac79
magne-matias <magnematias01@gmail.com>
2021-06-14 23:30:43 -0300

    commit

--- src/App.js
+++ src/App.js
@@ -2,15 +2,17 @@ import React from "react";
 import {Header} from "./components/Header"
 import 'boxicons';
 
-import {BrowserRouter as Router} from "react-router-dom"
+import {BrowserRouter as Route} from "react-router-dom"
 import Paginas from './components/Pages'
 
 
 function App() {
   return (
     <div className="App">
-      <Header/>
-      
+      <Route>
+        <Header/>
+        <Paginas/>
+      </Route>
     </div>
     
   );
