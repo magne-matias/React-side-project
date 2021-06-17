71f55936a6a68370888cf31c7a1f5673d503538a
magne-matias <magnematias01@gmail.com>
2021-06-17 20:08:07 -0300

    commit

--- src/components/Carrito/index.js
+++ src/components/Carrito/index.js
@@ -4,15 +4,15 @@ import Card from '../../images/g3.jpg'
 
 export const Carrito =()=>{
     return(
-        <div className="carritos">
-            <div className="carrito">
+        <div className="carritos show">
+            <div className="carrito show">
                 <div className="carrito_close">
                     <box-icon name="x"></box-icon>
                 </div>
                 <h2>Su carrito mi pana</h2>
                 <div className="carrito_center">
                     <div className="carrito_item">
-                        <img src={Card} alt="" width="100px"/>
+                        <img src={Card} alt=""/>
                         <div>
                             <h3>title</h3>
                             <p className="price">$350</p>
@@ -26,6 +26,11 @@ export const Carrito =()=>{
                             <box-icon name="trash"></box-icon>
                         </div>
                     </div>
+                </div>
+
+                <div className="carrito_footer">
+                    <h3>total: $2353</h3>
+                    <button className="btn" >Payment</button>
                 </div>
             </div>
         </div>
