928780b4dc83f5b869df23e5da178e52c3eb210c
magne-matias <magnematias01@gmail.com>
2021-06-21 20:44:00 -0300

    commit

--- src/components/Carrito/index.js
+++ src/components/Carrito/index.js
@@ -6,7 +6,8 @@ import {DataContext} from '../../context/DataProvider'
 export const Carrito =()=>{
 
     const value = useContext(DataContext)
-    const [menu,setMenu] =value.menu
+    const [menu,setMenu] =value.menu;
+    const [carrito, setCarrito]=value.carrito;
 
     const tooglefalse =()=>{
         setMenu(false);
@@ -22,70 +23,27 @@ export const Carrito =()=>{
                     <box-icon name="x"></box-icon>
                 </div>
                 <h2>Su carrito mi pana</h2>
+                
                 <div className="carrito_center">
-                    <div className="carrito_item">
-                        <img src={Card} alt=""/>
-                        <div>
-                            <h3>Guitar 1</h3>
-                            <p className="price">$350</p>
-                        </div>
-                        <div>
-                            <box-icon name="up-arrow" type="solid" ></box-icon>
-                            <p className="cantidad">1</p>
-                            <box-icon name="down-arrow" type="solid" ></box-icon>
-                        </div>
-                        <div className="remove_item">
-                            <box-icon name="trash"></box-icon>
-                        </div>
-                    </div>
-
-                    <div className="carrito_item">
-                        <img src={Card} alt=""/>
-                        <div>
-                            <h3>Guitar 1</h3>
-                            <p className="price">$350</p>
-                        </div>
-                        <div>
-                            <box-icon name="up-arrow" type="solid" ></box-icon>
-                            <p className="cantidad">1</p>
-                            <box-icon name="down-arrow" type="solid" ></box-icon>
-                        </div>
-                        <div className="remove_item">
-                            <box-icon name="trash"></box-icon>
-                        </div>
-                    </div>
-
-                    <div className="carrito_item">
-                        <img src={Card} alt=""/>
-                        <div>
-                            <h3>Guitar 1</h3>
-                            <p className="price">$350</p>
-                        </div>
-                        <div>
-                            <box-icon name="up-arrow" type="solid" ></box-icon>
-                            <p className="cantidad">1</p>
-                            <box-icon name="down-arrow" type="solid" ></box-icon>
-                        </div>
-                        <div className="remove_item">
-                            <box-icon name="trash"></box-icon>
-                        </div>
-                    </div>
-
-                    <div className="carrito_item">
-                        <img src={Card} alt=""/>
-                        <div>
-                            <h3>Guitar 1</h3>
-                            <p className="price">$350</p>
-                        </div>
-                        <div>
-                            <box-icon name="up-arrow" type="solid" ></box-icon>
-                            <p className="cantidad">1</p>
-                            <box-icon name="down-arrow" type="solid" ></box-icon>
-                        </div>
-                        <div className="remove_item">
-                            <box-icon name="trash"></box-icon>
-                        </div>
-                    </div>
+                    {
+                        carrito.map((producto)=>{
+                            <div className="carrito_item">
+                                <img src={producto.images} alt=""/>
+                                <div>
+                                    <h3> {producto.title}</h3>
+                                    <p className="price">${producto.price} </p>
+                                </div>
+                                <div>
+                                    <box-icon name="up-arrow" type="solid" ></box-icon>
+                                    <p className="cantidad"> {producto.cantidad} </p>
+                                    <box-icon name="down-arrow" type="solid" ></box-icon>
+                                </div>
+                                <div className="remove_item">
+                                    <box-icon name="trash"></box-icon>
+                                </div>
+                            </div>
+                        })
+                    }
                 </div>
 
                 <div className="carrito_footer">
